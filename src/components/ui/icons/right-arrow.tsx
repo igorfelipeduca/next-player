a51594891a7c9c8d174ea27d72063578064a0cdf
Igor F. Duca <yelldutz@gmail.com>
2024-06-23 01:49:42 -0300

    feat: homepage

---
+++ src/components/ui/icons/right-arrow.tsx
@@ -0,0 +1,23 @@
+import { ComponentProps } from "react";
+
+type RightArrowProps = ComponentProps<"svg">;
+
+export default function RightArrow({ className, ...props }: RightArrowProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      className={className}
+      {...props}
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+      />
+    </svg>
+  );
+}
